5c36f7e19a476b5e528c52d0c0626e1797078b76
俞俞 <hanabiranoshitaniumore@gmail.com>
2018-10-29 00:20:09 +0800

    加入專案檔案。

---
+++ src/Tsubaki.Addons.Process/Process.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Tsubaki.Addons.Process
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Tsubaki.ModuleBlocks;
+    using Tsubaki.ModuleBlocks.Enums;
+    using Tsubaki.ModuleBlocks.Metadata;
+    using System.Diagnostics;
+    using CommandLine;
+
+    [Module("Process")]
+    public class Proc : ModuleBase
+    {
+        protected override bool ExecuteImpl(string[] args, ref object callback)
+        {
+            var result = Parser.Default.ParseArguments<Info>(args);
+            try
+            {
+                result.WithParsed(info =>
+
+                {
+                    var pinfo = new ProcessStartInfo();
+                    pinfo.FileName = info.Exe;
+                    pinfo.Arguments = info.Target;
+                    pinfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    
+                    Process.Start(pinfo);
+
+                });
+                callback = "success";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            callback = "failure";
+            return false;
+
+        }
+
+        public override ModuleScopes Scopes => throw new NotImplementedException();
+
+        private class Info
+        {
+            [Option('t', "target", Required = true)]
+            public string Target { get; private set; }
+
+            [Option('x', "exe", Required = true)]
+            public string Exe { get; private set; }
+        }
+    }
+}
---
+++ src/Tsubaki.Addons.Weather/Weather.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Tsubaki.Addons.Weather
+{
+    using Tsubaki.ModuleBlocks;
+    using  Tsubaki.ModuleBlocks.Metadata;
+    using Tsubaki.ModuleBlocks.Enums;
+    using Ryis.Surround;
+
+
+    using Newtonsoft.Json;
+    using System.Net.Http;
+    using System.IO;
+    using System.Reflection;
+    using System.Diagnostics;
+
+    [Module("Weather")]
+    public sealed class Weather : ModuleBase 
+    { 
+        public override ModuleScopes Scopes => ModuleScopes.None;
+        
+        protected override bool ExecuteImpl(string[] args, ref object callback)
+        {
+            var fetch = Surround.Service.FetchAsync().Result;
+
+            callback = fetch.ToString();
+
+            return true;
+        }
+    }   
+}
